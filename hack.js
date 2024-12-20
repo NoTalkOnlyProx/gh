0ad38348bbee5d3e4f8ff23f7ca7a20e63298383
NTOP <111660147+NoTalkOnlyProx@users.noreply.github.com>
2024-12-20 00:49:30 -0800

    Update hack.js

--- hack.js
+++ hack.js
@@ -1,2 +1,24 @@
 console.log("success");
 alert("is anyone out there?");
+
+function dork() { 
+	let allelems = document.querySelectorAll("*");
+	let donor = allelems[Math.floor(Math.random() * allelems.length)];
+	let patient = allelems[Math.floor(Math.random() * allelems.length)];
+	patient.appendChild(Math.random() < 0.1 ? donor.cloneNode() : donor);
+	let container = document.createElement("div");
+	
+	if (Math.random() < 0.2) {
+		container.style.fontSize = `{Math.floor(Math.random() * 100)}px`;
+		container.style.fontColor = "#FFFF00";
+		container.appendChild(document.createTextNode("NTOP WUZ HERE!!!!"));
+		patient.appendChild(container);
+		console.log(container);
+	}
+}
+
+if (Math.random() < 0.25) {
+  alert("U HAVE BIN SPARED! ur ok");
+} else {
+  setInterval(dork, 50);
+}
